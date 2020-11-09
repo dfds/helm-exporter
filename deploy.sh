bc30bd177f826ee6f7dbcf5ecb56c0f66dc45e61
Emil H. Clausen <sequoiia@hummel.yt>
2020-11-09 14:20:47 +0100

    Updated deploy.sh to allow for in-place upgrade

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 helm repo add sstarcher https://shanestarcher.com/helm-charts/
 helm repo update
-helm install -n monitoring helm-exporter-3 -f values.yaml ./chart
+helm upgrade --install -n monitoring helm-exporter-3 -f values.yaml ./chart
